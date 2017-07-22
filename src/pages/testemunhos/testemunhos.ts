7ce05276489da68e11a974ba5ac91ea77f68799c
luis-gustavo <luis.gustavo3013@hotmail.com>
2017-07-22 14:29:35 -0300

    Criado validacao para testemunhos

--- src/pages/testemunhos/testemunhos.ts
+++ src/pages/testemunhos/testemunhos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the TestemunhosPage page.
@@ -17,10 +18,16 @@ export class TestemunhosPage {
 
     testemunhos: FirebaseListObservable<any>;
     testemunho: {nome: string, telefone: string, email: string, testemunho: string, publico: boolean} = {nome: '', telefone: '', email: '', testemunho: '', publico: false};
+    testemunhoForm: FormGroup;
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public db: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public db: AngularFireDatabase, public formBuilder: FormBuilder) {
       this.testemunhos = db.list("/testemunhos");
+      this.testemunhoForm = formBuilder.group({
+        'nome': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(30)])],
+        'telefone': [null, Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(15)])],
+        'email': [null, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(40)])],
+        'testemunho': [null, Validators.compose([Validators.required, Validators.minLength(20), Validators.maxLength(400)])]
+      })
   }
 
   enviar() {
