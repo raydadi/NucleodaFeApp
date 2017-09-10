c2a4e244e7a13a36c81281a75135ca683cec6275
gustavo <gustavo@localhost.localdomain>
2017-09-10 20:41:39 -0300

    pagina culto online finalizada

--- src/pages/culto-online/culto-online.ts
+++ src/pages/culto-online/culto-online.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 /**
  * Generated class for the CultoOnlinePage page.
@@ -13,12 +14,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'culto-online.html',
 })
 export class CultoOnlinePage {
+    msg: FirebaseObjectObservable<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase,) {
+      this.msg = db.object("/culto-online");
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CultoOnlinePage');
   }
 
+  openExternalBrowser() {
+      window.open("https://www.youtube.com/user/nucleodafe/live", '_system', 'location=no');
+  }
 }
