ba6070c8d88d8999cea1969a5e61d263a9a6782c
gustavo <gustavo@localhost.localdomain>
2017-10-01 21:17:04 -0300

    como contribuir alterado

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const URL_BACKEND= 'https://parseserver-nfmobile.rhcloud.com/';
 export const URL_BACKEND_CRUD= URL_BACKEND + 'parse/classes';
 export const APP_ID= 'nucleodafe-X2iC';
-export const APP_VERSION= '2.0';
+export const APP_VERSION= '2.0.0';
 export const FIREBASE_CONFIG = {
     apiKey: "AIzaSyAGaLrUXimZ8X2r9QhUzTJgWq65OiaWTcg",
     authDomain: "nfmobile-f334b.firebaseapp.com",
