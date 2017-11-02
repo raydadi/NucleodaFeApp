92315be7e083f7e63eb0d68de47e3686d869fd4c
gustavo <gustavo@localhost.localdomain>
2017-11-02 16:26:26 -0200

    finalizado detalhes dos pequenos nucleos

--- src/pages/pequeno-nucleo/pequeno-nucleo-detail/pequeno-nucleo-detail.ts
+++ src/pages/pequeno-nucleo/pequeno-nucleo-detail/pequeno-nucleo-detail.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
 
 @IonicPage()
 @Component({
@@ -8,9 +10,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PequenoNucleoDetailPage {
 
-    pequenoNucleo: any;
+  pequenoNucleo: any;
+  imgsource: any;
+  showSpinner: boolean = true;
+  showTitle: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public zone: NgZone) {
       this.pequenoNucleo = this.navParams.get('data');
+
+      firebase.storage().ref().child(this.pequenoNucleo.fotoRef).getDownloadURL().then((url) => {
+          this.zone.run(() => {
+              this.imgsource = url;
+          })
+      })
+  }
+
+  showSpinnerMeth() {
+      this.showSpinner = false;
+      this.showTitle = true;
   }
 }
