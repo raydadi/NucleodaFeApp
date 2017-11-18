fb2a13d5deeb60d0936d4ccaef38d169d06408e3
gustavo <gustavo@localhost.localdomain>
2017-11-17 22:41:34 -0200

    configurado para enviar email para o testemunho e pedido de oração

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,3 @@
-//  Create and Deploy Your First Cloud Functions
-//  https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
@@ -13,6 +6,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 // Configure the email transport using the default SMTP transport and a GMail account.
 // For other types of transports such as Sendgrid see https://nodemailer.com/transports/
 // TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
@@ -57,36 +51,118 @@ const mailTransport = nodemailer.createTransport({
 });
 
 // Sends an email confirmation when a user changes his mailing list subscription.
-exports.sendEmail = functions.database.ref('/users/{uid}').onWrite(event => {
+exports.sendEmailTestemunho = functions.database.ref('/testemunhos/{id}').onWrite(event => {
   const snapshot = event.data;
   const val = snapshot.val();
 
-  if (!snapshot.changed('subscribedToMailingList')) {
-    return;
-  }
-
   const mailOptions = {
-    from: '"Spammy Corp." <noreply@firebase.com>',
-    to: val.email
+    from: 'Núcleo da Fé <noreplycontato@gmail.com>'
   };
 
-  // The user just subscribed to our newsletter.
-  if (val.subscribedToMailingList) {
-    mailOptions.subject = 'Thanks and Welcome!';
-    mailOptions.text = 'Thanks you for subscribing to our newsletter. You will receive our next weekly newsletter.';
+  mailOptions.to = val.email;
+  mailOptions.subject = 'Recebemos seu testemunho!';
+  mailOptions.html = 'Olá ' + val.nome + ',<br><br>Nós recebemos o seu testemunho! Gostaríamos de agradecer por compartilhar as bençãos que o Senhor tem derramado sobre sua vida!<br> Desejamos que mais milagres como estes te acompanhem em cada novo dia!<br><br>Núcleo da Fé';
+
     return mailTransport.sendMail(mailOptions).then(() => {
-      console.log('New subscription confirmation email sent to:', val.email);
+      console.log('Novo email enviado para:', val.email);
     }).catch(error => {
-      console.error('There was an error while sending the email:', error);
+      console.error('Error ao enviar email:', error);
     });
-  }
 
   // The user unsubscribed to the newsletter.
-  mailOptions.subject = 'Sad to see you go :`(';
-  mailOptions.text = 'I hereby confirm that I will stop sending you the newsletter.';
+  mailOptions.to = "luszczynski@gmail.com";
+  mailOptions.subject = 'Novo testemunho recebido!';
+  mailOptions.html = 'Um novo testemunho foi enviado por meio do aplicativo da igreja. Veja os dados abaixo: <br><br> Nome: ' + val.nome + '<br>Email: ' + val.email + '<br>Telefone: ' + val.telefone + '<br>Testemunho: ' + val.testemunho;
   return mailTransport.sendMail(mailOptions).then(() => {
-    console.log('New unsubscription confirmation email sent to:', val.email);
+     console.log('Novo email enviado para: luszczynski@gmail.com');
   }).catch(error => {
-    console.error('There was an error while sending the email:', error);
+     console.error('Error ao enviar email:', error);
+  });
+});
+
+exports.sendEmailPedidoOracao = functions.database.ref('/pedidos-oracao/{id}').onWrite(event => {
+  const snapshot = event.data;
+  const val = snapshot.val();
+
+  const mailOptions = {
+    from: 'Núcleo da Fé <noreplycontato@gmail.com>'
+  };
+
+  mailOptions.to = val.email;
+  mailOptions.subject = 'Recebemos seu pedido!';
+  mailOptions.html = 'Olá ' + val.nome + ',<br><br>Nós recebemos o seu pedido de oração! Acreditamos que a oração é uma poderosa ferramenta para tocar o coração de Deus. Encaminharemos o seu pedido de oração para a nossa equipe de intercessores. Eles se unirão a você em oração.<br><br>Núcleo da Fé';
+
+    return mailTransport.sendMail(mailOptions).then(() => {
+      console.log('Novo email enviado para:', val.email);
+    }).catch(error => {
+      console.error('Error ao enviar email:', error);
+    });
+
+  // The user unsubscribed to the newsletter.
+  mailOptions.to = "luszczynski@gmail.com";
+  mailOptions.subject = 'Nova oração recebida!';
+  mailOptions.html = 'Um novo pedido de oração foi recebido por meio do aplicativo da igreja. Veja os dados abaixo: <br><br> Nome: ' + val.nome + '<br>Email: ' + val.email + '<br>Telefone: ' + val.telefone + '<br>Pedido Oração: ' + val.pedido;
+  return mailTransport.sendMail(mailOptions).then(() => {
+     console.log('Novo email enviado para: luszczynski@gmail.com');
+  }).catch(error => {
+     console.error('Error ao enviar email:', error);
+  });
+});
+
+exports.sendEmailParticiparMinisterio = functions.database.ref('/ministerios/{id}/interessados').onWrite(event => {
+  const snapshot = event.data;
+  const val = snapshot.val();
+
+  const mailOptions = {
+    from: 'Núcleo da Fé <noreplycontato@gmail.com>'
+  };
+
+  mailOptions.to = val.email;
+  mailOptions.subject = 'Recebemos seu pedido!';
+  mailOptions.html = 'Olá ' + val.nome + ',<br><br>Nós recebemos o seu pedido de oração! Acreditamos que a oração é uma poderosa ferramenta para tocar o coração de Deus. Encaminharemos o seu pedido de oração para a nossa equipe de intercessores. Eles se unirão a você em oração.<br><br>Núcleo da Fé';
+
+    return mailTransport.sendMail(mailOptions).then(() => {
+      console.log('Novo email enviado para:', val.email);
+    }).catch(error => {
+      console.error('Error ao enviar email:', error);
+    });
+
+  // The user unsubscribed to the newsletter.
+  mailOptions.to = "luszczynski@gmail.com";
+  mailOptions.subject = 'Nova oração recebida!';
+  mailOptions.html = 'Um novo pedido de oração foi recebido por meio do aplicativo da igreja. Veja os dados abaixo: <br><br> Nome: ' + val.nome + '<br>Email: ' + val.email + '<br>Telefone: ' + val.telefone + '<br>Pedido Oração: ' + val.pedido;
+  return mailTransport.sendMail(mailOptions).then(() => {
+     console.log('Novo email enviado para: luszczynski@gmail.com');
+  }).catch(error => {
+     console.error('Error ao enviar email:', error);
+  });
+});
+
+exports.sendEmailParticiparPequenoNucleo = functions.database.ref('/pequenos-nucleos/{id}/interessados').onWrite(event => {
+  const snapshot = event.data;
+  const val = snapshot.val();
+
+  const mailOptions = {
+    from: 'Núcleo da Fé <noreplycontato@gmail.com>'
+  };
+
+  mailOptions.to = val.email;
+  mailOptions.subject = 'Recebemos seu pedido!';
+  mailOptions.html = 'Olá ' + val.nome + ',<br><br>Nós recebemos o seu pedido de oração! Acreditamos que a oração é uma poderosa ferramenta para tocar o coração de Deus. Encaminharemos o seu pedido de oração para a nossa equipe de intercessores. Eles se unirão a você em oração.<br><br>Núcleo da Fé';
+
+    return mailTransport.sendMail(mailOptions).then(() => {
+      console.log('Novo email enviado para:', val.email);
+    }).catch(error => {
+      console.error('Error ao enviar email:', error);
+    });
+
+  // The user unsubscribed to the newsletter.
+  mailOptions.to = "luszczynski@gmail.com";
+  mailOptions.subject = 'Nova oração recebida!';
+  mailOptions.html = 'Um novo pedido de oração foi recebido por meio do aplicativo da igreja. Veja os dados abaixo: <br><br> Nome: ' + val.nome + '<br>Email: ' + val.email + '<br>Telefone: ' + val.telefone + '<br>Pedido Oração: ' + val.pedido;
+  return mailTransport.sendMail(mailOptions).then(() => {
+     console.log('Novo email enviado para: luszczynski@gmail.com');
+  }).catch(error => {
+     console.error('Error ao enviar email:', error);
   });
 });
