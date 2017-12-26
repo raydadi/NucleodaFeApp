517c14254ad6af9bbcbf0794494e5a138789ccb8
gustavo <gustavo@localhost.localdomain>
2017-12-25 22:28:56 -0200

    criado provider de auth e configurado google login nele

--- src/pages/menu-pages/login/login.ts
+++ src/pages/menu-pages/login/login.ts
@@ -1,260 +1,244 @@
-import { Component, Inject } from '@angular/core';
-import { GooglePlus } from '@ionic-native/google-plus';
-import { NativeStorage } from '@ionic-native/native-storage';
-import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
-import { HomePage } from '../../home/home';
-import { Toast } from '@ionic-native/toast';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Component } from '@angular/core';
+// import { GooglePlus } from '@ionic-native/google-plus';
+// import { NativeStorage } from '@ionic-native/native-storage';
+// import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+// import { HomePage } from '../../home/home';
+// import { Toast } from '@ionic-native/toast';
+// import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
-import { IonicPage, NavController, NavParams, ActionSheetController, Platform, LoadingController, Events } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
+import { IonicPage } from 'ionic-angular';
+// import { Storage } from '@ionic/storage';
 
-import { FirebaseApp } from 'angularfire2';
+import { AuthServiceProvider } from '../../../providers/auth-service/auth-service';
 
-import * as firebase from 'firebase';
+// import { FirebaseApp } from 'angularfire2';
+// import * as firebase from 'firebase';
 
 @IonicPage()
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
-  user: User = null;
-  FB_APP_ID: number = 1091217954342738;
-  usersFire: FirebaseListObservable<any>;
-  userFire: FirebaseListObservable<any>;
+  // user: User = null;
+  // FB_APP_ID: number = 1091217954342738;
+  // usersFire: FirebaseListObservable<any>;
+  // userFire: FirebaseListObservable<any>;
 
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public actionSheetCtrl: ActionSheetController,
-    public platform: Platform,
-    public loadingCtrl: LoadingController,
-    public googlePlus: GooglePlus,
-    public facebook: Facebook,
-    public nativeStorage: NativeStorage,
-    @Inject(FirebaseApp) firebaseApp: any,
-    private toast: Toast,
-    private storage: Storage,
-    public events: Events,
-    public db: AngularFireDatabase
-
+    // public navCtrl: NavController,
+    // public navParams: NavParams,
+    // public actionSheetCtrl: ActionSheetController,
+    // public platform: Platform,
+    // public loadingCtrl: LoadingController,
+    // public googlePlus: GooglePlus,
+    // public facebook: Facebook,
+    // public nativeStorage: NativeStorage,
+    // @Inject(FirebaseApp) firebaseApp: any,
+    // private toast: Toast,
+    // private storage: Storage,
+    // public events: Events,
+    // public db: AngularFireDatabase,
+    public authProvider: AuthServiceProvider
   ) {
-    this.user = new User();
-    this.usersFire = db.list("/usuarios");
+    // this.user = new User();
+    // this.usersFire = db.list("/users");
   }
 
   doGoogleLogin() {
-
-    let nav = this.navCtrl;
-
-    let loading = this.loadingCtrl.create({
-      content: 'Aguarde...'
-    });
-
-    loading.present();
-
-    this.googlePlus.login({
-      'scopes': '',
-      'webClientId': '1086236008019-8orpu99bbsuti181ua0tq70tdl5c1879.apps.googleusercontent.com',
-      'offline': true
-    }).then((user) => {
-      const firecreds = firebase.auth.GoogleAuthProvider.credential(user.idToken);
-
-      firebase.auth().signInWithCredential(firecreds).then((res) => {
-        this.user.nome = user.displayName;
-        this.user.email = user.email;
-        this.user.fotoUrl = user.imageUrl;
-        this.user.id = user.idToken;
-
-        this.nativeStorage.getItem('device_token').then((data) => {
-          this.user.deviceToken = data;
-        }, (error) => {
-          alert('Erro ao conseguir device token' + error);
-        });
-
-        this.nativeStorage.setItem('user', this.user).then(() => {
-          nav.setRoot(HomePage);
-
-          this.userFire = this.db.list("/usuarios", {
-            query: {
-              orderByChild: 'id',
-              equalTo: this.user.id
-            }
-          });
-
-		  this.userFire.subscribe(data => {
-              console.log(data);
-              // Se já existe usuario
-              if (data.length > 0) {
-
-              }
-              else {
-                this.usersFire.push(this.user);
-              }
-          });
-
-          this.toast.show(`Logado com sucesso!`, 'short', 'bottom').subscribe(toast => { });
-          this.events.publish('login:changed', this.user);
-
-        }, (error) => {
-          this.toast.show(`Ocorreu um erro ao persistir os dados!`, 'short', 'bottom').subscribe(toast => { });
-        })
-
-      }).catch((err) => {
-        alert('Autenticação falhou: ' + err);
-        //this.toast.show(`Error ao logar!`, 'short', 'bottom').subscribe(toast => { });
-      })
-
-      loading.dismiss();
-
-    }, (error) => {
-      loading.dismiss();
-      this.toast.show(`Error ao logar-se com Google!`, 'short', 'center').subscribe(toast => { });
-    });
-
+      this.authProvider.doGoogleLogin();
   }
 
-  // doGoogleLogout() {
-  // 	this.googlePlus.logout().then((response) => {
-  // 		this.nativeStorage.remove('user');
-  // 		this.user = null;
-  // 		this.toast.show(`Usuário desconectado!`, 'short', 'bottom').subscribe(toast => { });
-  // 	}, (error) => {
-  // 		console.log(error);
-  // 		this.toast.show(`Erro ao desconectar!`, 'short', 'bottom').subscribe(toast => { });
-  // 	})
+  // doGoogleLogin() {
+  //
+  //   let nav = this.navCtrl;
+  //
+  //   let loading = this.loadingCtrl.create({
+  //     content: 'Aguarde...'
+  //   });
+  //
+  //   loading.present();
+  //
+  //   this.googlePlus.login({
+  //     'scopes': '',
+  //     'webClientId': '1086236008019-8orpu99bbsuti181ua0tq70tdl5c1879.apps.googleusercontent.com',
+  //     'offline': true
+  // }).then((userGoogle) => { // Logado no Google com sucesso!
+  //
+  //     const firecreds = firebase.auth.GoogleAuthProvider.credential(userGoogle.idToken);
+  //
+  //     firebase.auth().signInWithCredential(firecreds).then((res) => { // Logado no Firebase com sucesso
+  //
+  //       this.user.nome = userGoogle.displayName;
+  //       this.user.email = userGoogle.email;
+  //       this.user.fotoUrl = userGoogle.imageUrl;
+  //       this.user.id = userGoogle.idToken;
+  //
+  //       this.nativeStorage.getItem('device_token').then((data) => {
+  //         this.user.deviceToken = data;
+  //       }, (error) => {
+  //         alert('Erro ao conseguir device token' + error);
+  //       });
+  //
+  //       this.nativeStorage.setItem('user', this.user).then(() => {
+  //         nav.setRoot(HomePage);
+  //
+  //         this.userFire = this.db.list("/users", {
+  //           query: {
+  //             orderByChild: 'id',
+  //             equalTo: this.user.id
+  //           }
+  //         });
+  //
+	// 	  this.userFire.subscribe(data => {
+  //
+  //             // Se já existe usuario, atualiza os dados
+  //             if (data.length > 0) {
+  //               console.log("Usuario já existe: ",data);
+  //               //this.usersFire.update(data.$key,data);
+  //             }
+  //             else {
+  //               this.usersFire.push(this.user);
+  //             }
+  //             this.events.publish('login:changed', this.user);
+  //         });
+  //
+  //         this.toast.show(`Logado com sucesso!`, 'short', 'bottom').subscribe(toast => { });
+  //         this.events.publish('login:changed', this.user);
+  //
+  //       }, (error) => {
+  //         this.toast.show(`Ocorreu um erro ao persistir os dados!`, 'short', 'bottom').subscribe(toast => { });
+  //       })
+  //
+  //     }).catch((err) => {
+  //       alert('Autenticação falhou: ' + err);
+  //       //this.toast.show(`Error ao logar!`, 'short', 'bottom').subscribe(toast => { });
+  //     })
+  //
+  //     loading.dismiss();
+  //
+  //   }, (error) => {
+  //     loading.dismiss();
+  //     this.toast.show(`Error ao logar-se com Google!`, 'short', 'center').subscribe(toast => { });
+  //     console.log(error);
+  //   });
+  //
   // }
 
   doFacebookLogin() {
 
-    let permissions = new Array<string>();
-    let nav = this.navCtrl;
-
-    let loading = this.loadingCtrl.create({
-      content: 'Aguarde...'
-    });
-
-    loading.present();
-
-    permissions = [
-      'public_profile',
-      'user_friends',
-      'email'
-      // 'user_about_me',
-      // 'user_birthday',
-      // 'user_location',
-      // 'user_education_history',
-      // 'user_hometown',
-      // 'user_relationship_details',
-      // 'user_relationships',
-      // 'user_religion_politics',
-      // 'user_work_history'
-    ];
-
-    this.facebook.login(permissions).then((response) => {
-
-      let userId = response.authResponse.userID;
-      let params = new Array<string>();
-      let accessToken = response.authResponse.accessToken;
-
-      //Getting name and gender properties
-      this.facebook.api("/me?fields=name,gender,email", params).then((user) => {
-
-        const firecreds = firebase.auth.FacebookAuthProvider.credential(accessToken);
-
-        firebase.auth().signInWithCredential(firecreds).then((res) => {
-
-          this.user.nome = user.name;
-          this.user.email = user.email;
-          this.user.fotoUrl = "https://graph.facebook.com/" + userId + "/picture?type=large";
-          this.user.id = userId;
-
-          this.nativeStorage.getItem('device_token').then((data) => {
-            this.user.deviceToken = data;
-          }, (error) => {
-            alert('Erro ao conseguir device token' + error);
-          });
-
-          this.nativeStorage.setItem('user', this.user).then(() => {
-            nav.setRoot(HomePage);
-
-            this.userFire = this.db.list("/usuarios", {
-              query: {
-                orderByChild: 'id',
-                equalTo: this.user.id
-              }
-            });
-
-            this.userFire.subscribe(data => {
-                console.log(data);
-                // Se já existe usuario
-                if (data.length > 0) {
-
-                }
-                else {
-                  this.usersFire.push(this.user);
-                }
-            });
-
-            this.events.publish('login:changed', this.user);
-            this.toast.show(`Logado com sucesso!`, 'short', 'bottom').subscribe(toast => { });
-
-          }, (error) => {
-            this.toast.show(`Ocorreu um erro ao persistir os dados!`, 'short', 'bottom').subscribe(toast => { });
-          })
-
-        }).catch((err) => {
-
-          if(err.message.indexOf('An account already exists') >= 0) {
-              console.log(err);
-              // firebase.auth()
-              //    .currentUser
-              //    .link(credential)
-              //    .then((user) => {
-              //      console.log("Anonymous account successfully upgraded", user);
-              //    })
-              //    .catch((error) => {
-              //      console.log("Error upgrading anonymous account", error);
-              //    });
-          }
-          else {
-              alert('Autenticação falhou:' + err);
-              console.log(err);
-          }
-          //this.toast.show(`Erro ao conectar com facebook!`, 'short', 'bottom').subscribe(toast => { });
-        })
-      })
-
-      loading.dismiss();
-
-    }, (error) => {
-      loading.dismiss();
-      console.log(error);
-      this.toast.show(`Erro ao conectar com facebook!`, 'short', 'bottom').subscribe(toast => { });
-    });
+  //   let permissions = new Array<string>();
+  //   let nav = this.navCtrl;
+  //
+  //   let loading = this.loadingCtrl.create({
+  //     content: 'Aguarde...'
+  //   });
+  //
+  //   loading.present();
+  //
+  //   permissions = [
+  //     'public_profile',
+  //     'user_friends',
+  //     'email'
+  //     // 'user_about_me',
+  //     // 'user_birthday',
+  //     // 'user_location',
+  //     // 'user_education_history',
+  //     // 'user_hometown',
+  //     // 'user_relationship_details',
+  //     // 'user_relationships',
+  //     // 'user_religion_politics',
+  //     // 'user_work_history'
+  //   ];
+  //
+  //   this.facebook.login(permissions).then((response) => {
+  //
+  //     let userId = response.authResponse.userID;
+  //     let params = new Array<string>();
+  //     let accessToken = response.authResponse.accessToken;
+  //
+  //     //Getting name and gender properties
+  //     this.facebook.api("/me?fields=name,gender,email", params).then((user) => {
+  //
+  //       const firecreds = firebase.auth.FacebookAuthProvider.credential(accessToken);
+  //
+  //       firebase.auth().signInWithCredential(firecreds).then((res) => {
+  //
+  //         this.user.nome = user.name;
+  //         this.user.email = user.email;
+  //         this.user.fotoUrl = "https://graph.facebook.com/" + userId + "/picture?type=large";
+  //         this.user.id = userId;
+  //
+  //         this.nativeStorage.getItem('device_token').then((data) => {
+  //           this.user.deviceToken = data;
+  //         }, (error) => {
+  //           alert('Erro ao conseguir device token' + error);
+  //         });
+  //
+  //         this.nativeStorage.setItem('user', this.user).then(() => {
+  //           nav.setRoot(HomePage);
+  //
+  //           this.userFire = this.db.list("/users", {
+  //             query: {
+  //               orderByChild: 'id',
+  //               equalTo: this.user.id
+  //             }
+  //           });
+  //
+  //           this.userFire.subscribe(data => {
+  //               // Se já existe usuario
+  //               if (data.length > 0) {
+  //                 console.log("Usuario já existe: ",data);
+  //               }
+  //               else {
+  //                 this.usersFire.push(this.user);
+  //               }
+  //           });
+  //
+  //           this.events.publish('login:changed', this.user);
+  //           this.toast.show(`Logado com sucesso!`, 'short', 'bottom').subscribe(toast => { });
+  //
+  //         }, (error) => {
+  //           this.toast.show(`Ocorreu um erro ao persistir os dados!`, 'short', 'bottom').subscribe(toast => { });
+  //         })
+  //
+  //       }).catch((err) => {
+  //
+  //         if(err.message.indexOf('An account already exists') >= 0) {
+  //             console.log(err);
+  //             // firebase.auth()
+  //             //    .currentUser
+  //             //    .link(credential)
+  //             //    .then((user) => {
+  //             //      console.log("Anonymous account successfully upgraded", user);
+  //             //    })
+  //             //    .catch((error) => {
+  //             //      console.log("Error upgrading anonymous account", error);
+  //             //    });
+  //         }
+  //         else {
+  //             alert('Autenticação falhou:' + err);
+  //             console.log(err);
+  //         }
+  //         //this.toast.show(`Erro ao conectar com facebook!`, 'short', 'bottom').subscribe(toast => { });
+  //       })
+  //     })
+  //
+  //     loading.dismiss();
+  //
+  //   }, (error) => {
+  //     loading.dismiss();
+  //     console.log(error);
+  //     this.toast.show(`Erro ao conectar com facebook!`, 'short', 'bottom').subscribe(toast => { });
+  //   });
   }
-
-  // doFacebookLogout() {
-  // 	var nav = this.navCtrl;
-  // 	this.facebook.logout()
-  // 		.then((response) => {
-  // 			//user logged out so we will remove him from the NativeStorage
-  // 			this.nativeStorage.remove('user');
-  // 			this.user = null;
-  // 			this.toast.show(`Usuário desconectado!`, 'short', 'bottom').subscribe(toast => { });
-  // 			// nav.push(LoginPage);
-  // 		}, (error) => {
-  // 			console.log(error);
-  // 			this.toast.show(`Erro ao desconectar!`, 'short', 'bottom').subscribe(toast => { });
-  // 		});
-  // }
 }
 
-export class User {
-  id: string;
-  nome: string;
-  email: string;
-  fotoUrl: string;
-  deviceToken: string;
-}
+// export class User {
+//   id: string;
+//   nome: string;
+//   email: string;
+//   fotoUrl: string;
+//   deviceToken: string;
+//   roles: Array<string> = ["anonimo"];
+// }
--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -4,42 +4,136 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { AngularFireAuth } from 'angularfire2/auth';
 // Do not import from 'firebase' as you'll lose the tree shaking benefits
-import * as firebase from 'firebase/app';
+import * as firebase from 'firebase';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { NativeStorage } from '@ionic-native/native-storage';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { Toast } from '@ionic-native/toast';
+import { Storage } from '@ionic/storage';
 
 
-/*
-  Generated class for the AuthServiceProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
 @Injectable()
 export class AuthServiceProvider {
-  private currentUser: firebase.User;
+  FB_APP_ID: number = 1091217954342738;
+  usersFirebase: FirebaseListObservable<any>;
+  userFirebase: FirebaseListObservable<any>;
+  currentUser: User;
 
-  constructor(public afAuth: AngularFireAuth) {
-    console.log('Hello AuthServiceProvider Provider');
-    afAuth.authState.subscribe((user: firebase.User) => this.currentUser = user);
+  constructor(
+      public googlePlus: GooglePlus,
+      public facebook: Facebook,
+      public nativeStorage: NativeStorage,
+      private toast: Toast,
+      private storage: Storage,
+      public db: AngularFireDatabase
+  ) {
+      this.usersFirebase = db.list("/users");
   }
 
-  get authenticated(): boolean {
-    return this.currentUser !== null;
+  doGoogleLogin() {
+    this.googlePlus.login({
+      'scopes': '',
+      'webClientId': '1086236008019-8orpu99bbsuti181ua0tq70tdl5c1879.apps.googleusercontent.com',
+      'offline': true
+  }).then((userGoogle) => { // Logado no Google com sucesso!
+      this.currentUser = new User();
+
+      const firecreds = firebase.auth.GoogleAuthProvider.credential(userGoogle.idToken);
+
+      firebase.auth().signInWithCredential(firecreds).then((res) => { // Logado no Firebase com sucesso
+
+        // this.userFirebase = this.getUserById(this.currentUser.id);
+        this.userFirebase = this.getUserRefByEmail(userGoogle.email);
+
+        this.userFirebase.subscribe(data => {
+
+            console.log("Usuario recuperado do banco: ", data);
+
+            // Se existe usuario
+              if(data.length > 0) {
+                this.usersFirebase.update(data[0].$key, this.currentUser);
+                console.log("Usuario já existe. Atualizando...", this.userFirebase);
+              }
+              // Se não existe, cria um novo
+              else {
+                  this.currentUser.nome = userGoogle.displayName;
+                  this.currentUser.email = userGoogle.email;
+                  this.currentUser.fotoUrl = userGoogle.imageUrl;
+                  this.currentUser.id = userGoogle.idToken;
+                  this.currentUser.deviceToken = this.getDeviceToken();
+
+                  console.log("Usuario não existe. Criando novo", this.currentUser);
+
+                  this.usersFirebase.push(this.currentUser);
+              }
+        });
+
+        this.toast.show(`Logado com sucesso!`, 'short', 'bottom').subscribe(toast => { });
+
+      }).catch((err) => {
+        console.log('Autenticação falhou: ' + err);
+        this.toast.show(`Error ao logar!`, 'short', 'bottom').subscribe(toast => { });
+      })
+
+    }, (error) => {
+      console.log('Autenticação falhou 2: ' + error);
+      this.toast.show(`Error ao logar!`, 'short', 'bottom').subscribe(toast => { });
+    });
   }
 
-  signInWithFacebook(): firebase.Promise<any> {
-    return this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  logout() {
+
+      this.googlePlus.logout().then((response) => {
+          this.toast.show(`Desconectado com sucesso!`, 'short', 'bottom').subscribe(toast => { });
+      }, function (error) {
+          console.log(error);
+      })
+
+      this.facebook.logout().then((response) => {
+          this.toast.show(`Desconectado com sucesso!`, 'short', 'bottom').subscribe(toast => { });
+      }, (error) => {
+          console.log(error);
+      })
+
+      firebase.auth().signOut();
+
+      this.currentUser = null;
   }
 
-  signOut(): void {
-    this.afAuth.auth.signOut();
+  getDeviceToken(): any {
+      this.nativeStorage.getItem('device_token').then((data) => {
+        return data;
+      }, (error) => {
+        alert('Erro ao conseguir device token' + error);
+      });
+      return null;
   }
 
-  displayName(): string {
-    if (this.currentUser !== null) {
-      //return this.currentUser.facebook.displayName;
-    } else {
-      return '';
-    }
+  getUserRefByEmail(email: any): any {
+      return this.db.list("/users", {
+        query: {
+          orderByChild: 'email',
+          equalTo: email
+        }
+    });
   }
 
+
+
+  // userExist(usersRef: FirebaseListObservable<any>): any {
+  //     usersRef.subscribe(data => {
+  //         // Se já existe usuario, atualiza os dados
+  //         return data.length > 0
+  //     });
+  // }
+}
+
+export class User {
+  id: string;
+  nome: string;
+  email: string;
+  fotoUrl: string;
+  deviceToken: string;
+  roles: Array<string> = ["anonimo"];
 }
