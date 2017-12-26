16fe7e56688a8969c97c0f5491d023f63c7f2b08
gustavo <gustavo@localhost.localdomain>
2017-12-25 22:29:10 -0200

    criada pagina de enquete

---
+++ src/pages/enquete/enquete.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-enquete',
+  templateUrl: 'enquete.html',
+})
+export class EnquetePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+}
